e442072fd316b4af7fa069b84b0dedbbc1beda72
ryann127 <ryann.goldberg127@gmail.com>
2022-07-28 21:24:08 -0600

    added more to map page

--- client/src/pages/Map.js
+++ client/src/pages/Map.js
@@ -1,4 +1,15 @@
+import React from "react";
+
 export default function Map() {
-    const ref = useRef
+    const ref = React.useRef(null);
+    const [map, setMap] = React.useState();
+
+    React.useEffect(()=>{
+        if (ref.current && !map) {
+            setMap(new window.google.maps.Map(ref.current, {}));
+        }
+}, [ref, map])
+
+return <div ref={ref} />
 }
 
