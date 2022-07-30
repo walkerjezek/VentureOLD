94f936a1d4d1580d7a65f9fb6efb6ca68167a214
walkerj <wpjezek@gmail.com>
2022-07-30 13:07:07 -0600

    activities page start

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -4,43 +4,36 @@ import { useMutation, useQuery } from '@apollo/client';
 
 
 const Landing = () => {
-  const { loading, data } = useQuery(QUERY_TECH);
-
-  const techList = data?.tech || [];
-
-  const [formData, setFormData] = useState({
-    tech1: 'JavaScript',
-    tech2: 'JavaScript',
-  });
-  let navigate = useNavigate();
-
-  const [createMatchup, { error }] = useMutation(CREATE_MATCHUP);
-
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-
-    try {
-      const { data } = await createMatchup({
-        variables: { ...formData },
-      });
-
-      navigate(`/matchup/${data.createMatchup._id}`);
-    } catch (err) {
-      console.error(err);
-    }
-
-  };
+  const { loading, data } = useQuery(QUERY_ACTIVITY);
 
 
-  
+
+
   return (
     <div>
-      
+      <div style="background-color: rgb(152, 216, 146);">
+        <section id="landing">
+            <div class="ventureLanding">
+                <h1>Hello! What do you want to do today?</h1>
+            </div>
+            <div class="activitiesList">
+                <div class="btn-group-vertical" role="group" aria-label="Basic example">
+                    <button type="button" class="btn btn-secondary">Mountain Biking</button>
+                    <button type="button" class="btn btn-secondary">Road Biking</button>
+                    <button type="button" class="btn btn-secondary">Skiing</button>
+                    <button type="button" class="btn btn-secondary">Snowboarding</button>
+                    <button type="button" class="btn btn-secondary">Hiking</button>
+                    <button type="button" class="btn btn-secondary">Climbing</button>
+                    <button type="button" class="btn btn-secondary">Rafting</button>
+                    <button type="button" class="btn btn-secondary">Kayaking</button>
+                    <button type="button" class="btn btn-secondary">Tubing</button>
+                    <button type="button" class="btn btn-secondary">Paddle Boarding</button>
+                    <button type="button" class="btn btn-secondary">Tennis</button>
+                    <button type="button" class="btn btn-secondary">Golf</button>
+                </div>
+            </div>
+        </section>
+</div>
     </div>
   );
 };
